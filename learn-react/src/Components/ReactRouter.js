991bc0e5f1b998fbfa3b72f36b72be1c876ac22f
uyetiskin <ufukyetiskin@hotmail.com>
2022-06-29 00:58:24 +0300

    Nesting Example

--- learn-react/src/Components/ReactRouter.js
+++ learn-react/src/Components/ReactRouter.js
@@ -1,9 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {useRouteMatch} from 'react'
+import { useParams } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch, Routes, NavLink } from 'react-router-dom'
 
 
+const NestingExample = (props) =>{
 
+    return(
+        <div>
+            <div>
+            <ul>
+            <li>
+                <NavLink to='/topic'>Topic</NavLink>
+            </li>
+            <li>
+                <NavLink to='/topics'> Topics</NavLink>
+            </li>
+        </ul>
+                <Routes>
+                    <Route  path='/topic' element={<Topic/>}/>
+                    <Route  path='/topics' element={<Topics/>}/>
+                </Routes>
+            </div>
+        </div>
+    )
+}
+
+function Topics(props){
+    return(
+        <div>
+            <h1>Topics</h1>
+        </div>
+    )
+}
+
+function Topic(props){
+    return(
+        <div>
+            <h1>Topic</h1>
+        </div>
+    )
+}
 
 
 // Home component
@@ -27,6 +65,9 @@ const NavBar = (props) => (
         </li>
         <li>
             <NavLink to='/challenges'>Challenges</NavLink>
+        </li>
+        <li>
+            <NavLink to='/nesting'>Topics</NavLink>
         </li>
     </ul>
 )
@@ -65,6 +106,7 @@ export default class ReactRoter extends React.Component {
                 <Route path='/about' element={<About/>} />
                 <Route path='/contact' element={<Contact/>} />
                 <Route path='/challenges' element={<Challenges/>} />
+                <Route path='/nesting' element={<NestingExample/>} />
                 <Route path='/' element={<Home/>} />
                 {/* Rotamız ve navigasyonumuz, rota bulunduğu sürece kusursuz çalışır. Ancak bir rota bulunamazsa son bileşene düşer. Bu sorunu önlemek için ayrı bir bulunamayan bileşen oluşturalım ve onu yönlendirmemizin içine koyalım. */}
                 <Route element={<NotFound/>} />
