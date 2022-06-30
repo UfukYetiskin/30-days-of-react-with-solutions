8ef6dd6c00d449229bdda8a955b535336660ced6
uyetiskin <ufukyetiskin@hotmail.com>
2022-06-30 23:48:29 +0300

    Nesting completed

--- learn-react/src/Components/ReactRouter.js
+++ learn-react/src/Components/ReactRouter.js
@@ -14,11 +14,22 @@ const Contact = () => <h1>Contact</h1>
 const AboutLayout = () => (
     <div>
         <h1>About Us</h1>
+        <ul>
+            <li>
+                <NavLink to='about' >About</NavLink>
+            </li>
+            <li>
+                <NavLink to='vizyon' >Vizyon</NavLink>
+            </li>
+            <li>
+                <NavLink to='misyon' >Misyon</NavLink>
+            </li>
+        </ul>
         <Outlet/>
     </div>
 )
 
-const About = () => (<h6>About sayfası</h6>)
+const About = () => (<h1>About sayfası</h1>)
 
 const Vizyon = () => (
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -77,7 +88,7 @@ export default class ReactRoter extends React.Component {
                 <Route path='/' element={<Home/>} />
                 <Route path = '/contact' element={<Contact/>} />
                 <Route path = '/about' element={<AboutLayout/>}>
-                    <Route index={true} element={<About/>} />
+                    <Route path='about' element={<About/>} />
                     <Route path='vizyon' element={<Vizyon/>} />
                     {/* <Route path='misyon/:url' element={<Misyon/>} />  yazarsak url'e yönlendirir bizi. Bu url almak için ise useParams hookunu kullanabiliriz. */}
                     <Route path='misyon/:url' element={<Misyon/>} />
