8378fbce0272eb69f67d51c3329e742400b50cb4
uyetiskin <ufukyetiskin@hotmail.com>
2022-07-02 23:50:35 +0300

    updated

--- learn-react/src/Components/HomeWork.js
+++ learn-react/src/Components/HomeWork.js
@@ -17,8 +17,8 @@ const HomeWork = () => {
             <h1>This work about the Cats API</h1>
             <ul>
                 {cats && cats.map((cat, index) => (
-                    <li style={{overflow : 'auto',border : '1px solid black',listStyleType : 'none', marginTop : '5%', marginLeft : '5%', padding : ' 2%'}} key={index}>
-                        <img src='https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg'  alt={cat.image}/>
+                    <li style={{overflow : 'auto',border : '1px solid black',listStyleType : 'none', marginTop : '5%', marginLeft : '5%', padding : ' 2%', width : '50%'}} key={index}>
+                        <img style={{width : '100%'}} src='https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg'  alt={cat.image}/>
                         <p>{cat.name}</p>
                         <p>{cat.origin}</p>
                         <p>Temperament: {cat.temperament}</p>
