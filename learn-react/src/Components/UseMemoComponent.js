a51224e2d02fb0f453e062364cc05393a762ed93
uyetiskin <ufukyetiskin@hotmail.com>
2022-07-08 00:24:31 +0300

    useCallback

--- learn-react/src/Components/UseMemoComponent.js
+++ learn-react/src/Components/UseMemoComponent.js
@@ -1,13 +1,15 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 /*usememo hooksu expensive fonksiyonlarımızı gereksiz yere render olmasını engeller, 
 tamamen performans optimize etmek için kullanılan bir hook */
 
-const Header = ({data}) => {
+
+const Header = ({data, increment}) => {
     console.log('Header Componenti render edildi.')
     return(
         <div>
             <h5>Header</h5>
-            <code>{JSON.stringify(data)}</code>
+            <code>{JSON.stringify(data)}</code><br></br>
+            <button onClick={increment}>Increment</button>
         </div>
     )
 }
@@ -16,6 +18,15 @@ const Header = ({data}) => {
 const UseMemoComponent = () => {
     
     const [number, setNumber] = useState(0);
+    const [text, setText] = useState("")
+
+    
+    //useCallback
+    //useMemo nun aksine aldığı işlevin sonucunu saklamak yerine işlevin kendisini saklar. deps olarak verilen değerleri değişmediği sürece de sakladığı işlevi döndürür.
+    const increment = useCallback(()=>{
+        setNumber((prevState) => prevState + 1)
+    }, [])
+
     //Object referansı her render edildiğinde değişeceği için Header componenti her zaman render olacaktı. Bunu engellemek için useMemo hookunu kullandık
     //eğer [] içersine bir değişen değişken verirsek (number) header componenti render olacaktır.
     const data = useMemo(()=>{
@@ -29,11 +40,12 @@ const UseMemoComponent = () => {
     return(
         <div style={{border: '3px solid orange', margin: '2%', padding : '2%'}}>
             <h1>This component includes UseMemo Hook</h1>
-            <Header data={data} funcDeneme={funcDeneme}/>
+            <Header increment={increment} data={data} funcDeneme={funcDeneme}/>
             <div>
                 <button onClick={()=> setNumber(number + 1)}>İncrease</button>
                 <span>{number}</span>
             </div>
+            <input value={text} onChange={(e)=> setText(e.target.value)} />
         </div>
     )
 }
