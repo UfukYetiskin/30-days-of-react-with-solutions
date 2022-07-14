86ef844e8280f20dc952cbb2395a97498648c354
uyetiskin <ufukyetiskin@hotmail.com>
2022-07-15 01:54:23 +0300

    usestate hooku ile city namei alınmaya çalışıldı.

--- learn-react/src/Components/weather/WeatherContext.js
+++ learn-react/src/Components/weather/WeatherContext.js
@@ -1,12 +1,25 @@
-import {createContext} from 'react'
+import {createContext, useState, useEffect} from 'react'
 
 const WheatherContext = createContext();
 
 export const  WheatherProvider = ({children}) => {
-    const weather = "weather"
+    const [data, setData] = useState(false)
+    const [city, setCity] = useState('Ankara')
+    const apiKey = 'a8f42045640d3c79798418f0074f1cd4'
+    const url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=tr`
+
     const values = {
-        weather
+        data,
+        setData,
+        city,
+        setCity,
     }
+    useEffect(()=> {
+        fetch(url)
+            .then((res)=> res.json())
+            .then((data) => setData(data))
+    }, [])
+    console.log(data)
     return(
         <WheatherContext.Provider value={values}>
             {children}
--- learn-react/src/Components/weather/Wheather.js
+++ learn-react/src/Components/weather/Wheather.js
@@ -1,15 +1,38 @@
-import { useContext } from "react"
+import { useContext} from "react"
 import WheatherContext from "./WeatherContext"
 
 
 
 const Weather = () => {
-    const data = useContext(WheatherContext)
-
+    const { data, setData, city, setCity} = useContext(WheatherContext)
+    
+    const handleClick = (e) => {
+        setCity(e.target.value)
+    }
+    const handleChange =(e) => {
+        const sehir = e.target.value
+        setCity(sehir)
+    }
     return(
     <div>
         <h1>Weather Component</h1>
-        {data.weather}
+        <hr/>
+        <div>
+            <input 
+                placeholder="City"
+                value={city}
+                type="text"
+                onChange={handleChange}
+            />
+            <button onClick={handleClick}>Göster</button>
+        </div>
+        <div>
+            Country: {data.sys.country}<br/>
+            City : {data.name}<br/>
+            Sıcaklık: {Math.floor(data.main.temp)}<br/>
+            Hissedilen Sıcaklık: {Math.floor(data.main.feels_like)}<br/>
+        </div>
+        <h1>{city}</h1>
     </div>
     )
 
